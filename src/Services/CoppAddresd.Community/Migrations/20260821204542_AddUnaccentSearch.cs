dae0743cfaf002971802e618ad9838d3549b0828
Kenma <kenmatwofour@gmail.com>
2026-08-21 15:46:22 -0500

    feat(community): add unaccent extension for accent-insensitive search
    - enables unaccent() in the community schema database

---
+++ src/Services/CoppAddresd.Community/Migrations/20260821204542_AddUnaccentSearch.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoppAddresd.Community.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUnaccentSearch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE EXTENSION IF NOT EXISTS unaccent;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
